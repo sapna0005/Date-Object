8f691bfd48d902c19602a79f91437239a0d2ad85
sapna0005 <sapna.sahani0005@gmail.com>
2025-01-02 22:54:15 +0530

    added date object

---
+++ date.js
@@ -0,0 +1,36 @@
+// let date=new Date();
+// console.log(date.toLocaleString());
+
+// let ans1=date.getFullYear() //2025
+// let ans2=date.getMonth()
+// let ans3=date.getDay()
+// let ans4=date.getHours()
+// let ans5=date.getMinutes()
+// let ans6=date.getSeconds()
+// let ans7=date.getMilliseconds()
+// console.log(ans1);
+// console.log(ans2);
+// console.log(ans3);
+// console.log(ans4);
+// console.log(ans5);
+// console.log(ans6);
+// console.log(ans7);
+
+// let clickk=()=>{
+//     let date=new Date();
+//     let ans=document.querySelector("#dates")
+//     ans.innerHTML= date.toLocaleString();
+    
+// }
+
+let clickk=()=>{
+    let arr=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+    let time=new Date();
+    let ans=document.querySelector("#dates")
+    ans.innerHTML=arr[time.getDay()]
+
+}
+
+
+
+
