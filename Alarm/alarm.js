c91ee43c63b5d20a37e9f40e5a01c1166d8883af
sapna0005 <sapna.sahani0005@gmail.com>
2025-01-05 14:51:37 +0530

    added alarm

--- Alarm/alarm.js
+++ Alarm/alarm.js
@@ -1,4 +1,21 @@
-let alarm=()=>{
-    document.querySelector("#inp").value
-    document.querySelector("#inp").value
+let alaram=()=>{
+ 
+    let show=document.querySelector("#display")
+    let inp=document.querySelector("#inp").value
+    let inter=setInterval(()=>{
+      let abcd=new Date()
+      let alaramtime=`${abcd.getHours().toString().padStart(2,"0")}:${abcd.getMinutes().toString().padStart(2,"0")}`
+      if (alaramtime==inp) {
+        show.innerHTML="Alarm is ringing"
+        document.querySelector("#aud").play()
+        
+        setTimeout(()=>{
+            document.querySelector("#aud").pause()
+            clearInterval(inter)
+        },7000)
+      }
+      else{
+        show.innerHTML="Alarm Set"
+      }
+    },1000)
 }
---
+++ Math Object/math.js
@@ -0,0 +1,7 @@
+
+
+// console.log(Math.PI);
+// console.log(Math.abs(-7));
+// console.log(1000+ Math.floor(Math.random()*9000));
+
+
