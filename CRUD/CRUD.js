1ea27cb665ed1db90669ac55f0e9d8c708478a57
sapna0005 <sapna.sahani0005@gmail.com>
2025-01-16 23:26:53 +0530

    added json

--- CRUD/CRUD.js
+++ CRUD/CRUD.js
@@ -1,9 +1,23 @@
-console.log("received");
-setTimeout(()=>{
-    console.log("process");
-},3000)
+// console.log("received");
+// setTimeout(()=>{
+//     console.log("process");
+// },3000)
 
-console.log("completed");
+// console.log("completed");
+
+let fetchData=async()=>{
+    let url="http://localhost:3000/doctor"
+    let res=await fetch(url,{method:"GET"})//method we are using here is method get
+    let data=await res.json()
+
+//    by default behavoiur of fetch is get
+
+console.log(data);
+// fetch method is aynchronous to convert it into syncronous we use async and await keyword
+
+
+}
+
 
 
 
