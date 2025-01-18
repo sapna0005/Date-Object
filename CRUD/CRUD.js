d64a31f8b6065316e7ccbec9f588904e57c130ac
sapna0005 <sapna.sahani0005@gmail.com>
2025-01-18 23:11:48 +0530

    added POST method

--- CRUD/CRUD.js
+++ CRUD/CRUD.js
@@ -26,15 +26,49 @@ data.map((e)=>{
          <td>${e.problem}</td>
          <td>${e.date}</td>
          <td>${e.price}</td>
-         <td onclick="deletee('${e.id}')">Delete</td>
+         <td>${e.id}</td>
+         <td onclick="deelete('${e.id}')">Delete</td>
       </tr>
     `
 })
 }
 
-let deletee=(id)=>{
+let deelete=(id)=>{
     let url=`http://localhost:3000/doctor/${id}`
     fetch(url,{method:"DELETE"})
+}
+
+let insertt=()=>{
+
+    let inpname=document.querySelector("#inpname").value;
+    let inpage=document.querySelector("#inpage").value;
+    let inpnumber=document.querySelector("#inpnumber").value;
+    let inpdisease=document.querySelector("#inpdisease").value;
+    let inpdate=document.querySelector("#inpdate").value;
+    let inpprice=document.querySelector("#inpprice").value;
+    
+    
+    let url='http://localhost:3000/car'
+    
+    fetch(url,{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify({
+    
+          name:inpname ,
+          age:inpage ,
+          number:inpnumber ,
+          problem:inpdisease ,
+          date:inpdate ,
+          price: inpprice
+    
+      })
+    
+    })
+    return false;
+    
 }
 
 
